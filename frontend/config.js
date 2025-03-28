c3123155d609b0a2ca81cd7b683a5300e7d1e178
Varshith <varshithsai52@gmail.com>
2025-03-28 16:11:58 +0530

    Added some android modifications

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'http:///192.168.1.8:5002', // Adjust this based on your server's IP
+  BASE_URL: 'http:///192.168.1.5:5002', // Adjust this based on your server's IP
 };
 
 export default CONFIG;
